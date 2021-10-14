07d109ad25fff511fe3990dd1af9eb3e82107ce3
karim-zaidi <90897995+karim-zaidi@users.noreply.github.com>
2021-10-14 12:22:12 +0200

    fix: delete comment

--- Building.py
+++ Building.py
@@ -1,6 +1,7 @@
 from DividerList import DividerList
 from Floor import Floor
 from Wall import Wall
+from Boundary import Boundary
 import matplotlib.pyplot as plt
 
 class Building():
@@ -22,16 +23,46 @@ class Building():
 
     def visualize(self):
         n = len(self.floors)
-        f, axes = plt.subplot(n, 1, figsize=(15*n, n), sharey = True)
 
-        for i, ax in enumerate(axes.flat):
+        # creating the plot
+        plt.figure(figsize=(5, 5*n))
+
+        for i in range(n):
             floor = self.floors[i]
-            ax.title('Floor %s ' % floor.name)
+            plt.title(f'Floor {floor.name}')
 
-            # display dividers
+            # display dividers and their id
             for div in floor.dividers :
-                # display only walls
+                # black line if wall
                 if isinstance(div, Wall):
-                    ax.plot(div.p1, div.p2)
+                    x1, y1, x2, y2 = div.p1, div.p2
+                    plt.plot((x1,x2), (y1, y2), color='black')
+
+                    for window in div.windows:
+                        x1, y1, x2, y2 = window.p1, window.p2
+                        plt.plot((x1,x2), (y1, y2), c='blue', linewidth = '2')
+                    
+                    for door in div.doors:
+                        x1, y1, x2, y2 = door.p1, door.p2
+                        plt.plot((x1,x2), (y1, y2), c='brown', linewidth = '2')    
+            
+                # gray point line if boundary
+                if isinstance(div, Boundary):
+                    x1, y1, x2, y2 = div.p1, div.p2
+                    plt.plot((x1,x2), (y1, y2), color='gray', linestyle = ':') # --
+                
+            plt.show()
 
 
+"""            
+plt.figure(figsize=(5,5*n))
+
+for i in range(n):
+    
+    plt.subplot(n+1, 1, i+1)
+    
+    plt.title('Floor %d' %i)
+    p1, p2 = [-1, 12], [1, 4]
+    plt.plot(p1, p2)
+plt.show()
+"""
