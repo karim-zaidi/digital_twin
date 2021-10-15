8dabdaa97f8f6c47f2a2b0ab8b5055f1a7bfce6d
karim-zaidi <90897995+karim-zaidi@users.noreply.github.com>
2021-10-15 15:22:58 +0200

    feat: add are in visualization and some getters

--- Floor.py
+++ Floor.py
@@ -4,12 +4,21 @@ from Zone import Zone
 
 class Floor():
 
-    def __init__(self, name, dividers = DividerList([]), areas = [], zones = []):
+    def __init__(self, name, dividers = [], areas = [], zones = []):
         self.name = name
         self.dividers = dividers
         self.areas = areas
         self.zones = zones
-
+    
+    def get_dividers(self):
+        return self.dividers
+    
+    def get_areas(self):
+        return self.areas
+    
+    def get_zones(self):
+        return self.zones
+    
     def get_divider_by_id(self, id):
         for d in self.dividers:
             if d.id == id:
@@ -17,4 +26,5 @@ class Floor():
         raise ValueError('There is no divider with such an id on this floor (%s)' %str(self.name))
 
     def add_divider(self, divider):
-        self.dividers.add_divider(divider)
+        self.dividers.append(divider)
+
