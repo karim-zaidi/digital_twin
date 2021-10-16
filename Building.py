5d204e9e0ff10d26baa115a9facdbd764104cd42
Maxime DUVAL <82819388+MaximeDuvalP@users.noreply.github.com>
2021-10-16 02:54:43 +0200

    feat: new methods

--- Building.py
+++ Building.py
@@ -5,6 +5,8 @@ from Boundary import Boundary
 from Window import Window
 from Door import Door
 
+# import matplotlib.pyplot as plt
+
 class Building():
 
     def __init__(self, name):
@@ -44,3 +46,50 @@ class Building():
         assert isinstance(wall, Wall), 'The id does not correspond to a wall but a boundary'
         door = Door(p1, p2)
         wall.add_door(door)
+
+    def visualize(self):
+        n = len(self.floors)
+        
+        # creating the plot
+        plt.figure(figsize=(5, 5*n))
+
+        for floor_name in self.floors.keys():
+            floor = self.floors[floor_name]
+            plt.title(f'Floor {floor.name}')
+
+            # display dividers and their id
+            for div in floor.get_dividers() :
+                
+                x1, x2 = div.get_x_coords()
+                y1, y2 = div.get_y_coords()
+
+                # display id
+                xmin, xmax = min(x1,x2), max(x1,x2)
+                ymin, ymax = min(y1,y2), max(y1,y2)
+                plt.text(0.1*xmin + 0.9*xmax, 0.1*ymin +0.9*ymax, str(div.id), c='black', style='italic', bbox={'facecolor': 'white'}, ha='center', va='center')
+                
+                # black line if wall
+                if isinstance(div, Wall):
+                    plt.plot((x1,x2), (y1, y2), color='black')
+
+                    for window in div.get_windows() :
+                        x_1, x_2 = window.get_x_coords()
+                        y_1, y_2 = window.get_y_coords()
+                        x_1, y_1, x_2, y_2 = window.p1, window.p2
+                        plt.plot((x1,x2), (y1, y2), c='blue', linewidth = '2')
+
+                        # display id
+                        x_min, x_max = min(x_1,x_2), max(x_1,x_2)
+                        y_min, y_max = min(y_1,y_2), max(y_1,y_2)
+                        plt.text(0.1*x_min + 0.9*x_max, 0.1*y_min +0.9*y_max, str(window.id), c='black', style='italic', bbox={'facecolor': 'white'}, ha='center', va='center')
+                    
+                    for door in div.get_doors() :
+                        x_1, x_2 = window.get_x_coords()
+                        y_1, y_2 = window.get_y_coords()
+                        plt.plot((x_1,x_2), (y_1, y_2), c='brown', linewidth = '2')    
+            
+                # gray point line if boundary
+                if isinstance(div, Boundary):
+                    plt.plot((x1,x2), (y1, y2), color='gray', linestyle = ':') # --
+
+            plt.show()
