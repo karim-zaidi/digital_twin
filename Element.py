11f9a2f5801c9e05a4faea7f80dc1ed84747e366
karim-zaidi <90897995+karim-zaidi@users.noreply.github.com>
2021-11-01 16:52:04 +0100

    feat: add setters/getters and some tests

--- Element.py
+++ Element.py
@@ -1,27 +1,64 @@
 class Element():
 
-    id = 0
+    __id = 0
 
     def __init__(self, p1, p2):
-        assert isinstance(p1, tuple) and isinstance(p2, tuple), 'The 2 coordinates points must be tuples'
+        assert isinstance(p1, tuple), f'p1 must be a tuple not a {type(p1).__name__}'
+        assert isinstance(p2, tuple), f'p2 must be a tuple not a {type(p2).__name__}'
         assert len(p1) == 2 and len(p2) == 2, 'Each coordinate point must have 2 coordinates'
-        assert isinstance(p1[0], int) or isinstance(p1[0], float), 'Coordinates must be int of float'
-        assert isinstance(p1[1], int) or isinstance(p1[1], float), 'Coordinates must be int of float'
-        assert isinstance(p2[0], int) or isinstance(p2[0], float), 'Coordinates must be int of float'
-        assert isinstance(p2[1], int) or isinstance(p2[1], float), 'Coordinates must be int of float'
+        assert isinstance(p1[0], (int, float)), 'Coordinates must be int of float'
+        assert isinstance(p1[1], (int, float)), 'Coordinates must be int of float'
+        assert isinstance(p2[0], (int, float)), 'Coordinates must be int of float'
+        assert isinstance(p2[1], (int, float)), 'Coordinates must be int of float'
 
-        self.p1 = p1
-        self.p2 = p2
+        self.__p1 = p1
+        self.__p2 = p2
         assert self.is_axis_aligned(), 'An element must either be aligned with the x or the y axis'
 
-        self.id = Element.id # Each element (door, boundary(=invisible/open wall), door, window) has a unique id
-        Element.id += 1
+        # Each element (door, boundary(=invisible/open wall), door, window) has a unique unchanged id
+        self.__id = Element.__id
+        Element.__id += 1
 
-    def is_axis_aligned(self):
-        return self.p1[0] == self.p2[0] or self.p1[1] == self.p2[1]
+    # id
+    @property
+    def id(self):
+        return self.__id
 
+    # p1
+    @property
+    def p1(self):
+        return self.__p1
+
+    @p1.setter
+    def p1(self, new_p1):
+        assert isinstance(new_p1, tuple), f'p1 must be a tuple not a {type(new_p1).__name__}'
+        assert len(new_p1) == 2, f'p1 must have 2 coordinates, not {len(new_p1)}'
+        assert isinstance(new_p1[0], (int, float)), f'Coordinates must be int of float {type(new_p1[0]).__name__}'
+        assert isinstance(new_p1[1], (int, float)), f'Coordinates must be int of float not {type(new_p1[1]).__name__}'
+        
+        self.__p1 = new_p1
+        assert self.is_axis_aligned(), 'An element must either be aligned with the x or the y axis'
+
+    # p2
+    @property
+    def p2(self):
+        return self.__p2
+
+    @p2.setter
+    def p2(self, new_p2):
+        assert isinstance(new_p2, tuple), f'p1 must be a tuple not a {type(new_p2).__name__}'
+        assert len(new_p2) == 2, f'p1 must have 2 coordinates, not {len(new_p2)}'
+        assert isinstance(new_p2[0], (int, float)), f'Coordinates must be int of float {type(new_p2[0]).__name__}'
+        assert isinstance(new_p2[1], (int, float)), f'Coordinates must be int of float not {type(new_p2[1]).__name__}'
+        self.__p2 = new_p2
+        assert self.is_axis_aligned(), 'An element must either be aligned with the x or the y axis'
+
+    # Other methods
     def get_x_coords(self):
-        return (self.p1[0], self.p2[0])
+        return (self.__p1[0], self.__p2[0])
 
     def get_y_coords(self):
-        return (self.p1[1], self.p2[1]) 
+        return (self.__p1[1], self.__p2[1])
+
+    def is_axis_aligned(self):
+        return self.__p1[0] == self.__p2[0] or self.__p1[1] == self.__p2[1]
--- Element_test.py
+++ Element_test.py
@@ -1,20 +1,61 @@
 import unittest
 from Element import Element
 
+
 class Element_test(unittest.TestCase):
 
-    def test_create_element(self):
-        element = Element((0,0), (0,0))
+    def test_0_create_element(self):
+        element = Element((0, 0), (0, 0))
         self.assertIsInstance(element, Element)
 
-    def test_id(self):
-        (x1, y1), (x2, y2) = (0, 0), (10,0)
-        element1 = Element((x1, y1), (x2, y2))
-        self.assertEqual(element1.id, 1)
+    def test_1_create_error(self):
+        with self.assertRaises(AssertionError):
+            Element([1, 0], [0, 0])
 
-        (x1, y1), (x2, y2) = (0, 0), (10,0)
+    def test_2_alignement_error(self):
+        with self.assertRaises(AssertionError):
+            Element((1, 1), (0, 0))
+
+    def test_3_id_increment(self):
+        (x1, y1), (x2, y2) = (0, 0), (10, 0)
+        element1 = Element((x1, y1), (x2, y2))
         element2 = Element((x1, y1), (x2, y2))
-        self.assertEqual(element2.id, 2)
+        self.assertTrue(element1.id == 1 and element2.id == 2)
+
+    def test_4_id_error(self):
+        (x1, y1), (x2, y2) = (0, 0), (10, 0)
+        element = Element((x1, y1), (x2, y2))
+        with self.assertRaises(AttributeError):
+            element.__id
+        with self.assertRaises(AttributeError):
+            element.id = 1
+
+    def test_5_attribut(self):
+        element = Element((0, 0), (10, 0))
+        self.assertTrue(element.p1 == (0, 0) and element.p2 == (10, 0))
+
+    def test_6_attribut_error(self):
+        element = Element((0, 0), (0, 0))
+        with self.assertRaises(AttributeError):
+            element.__p1
+            element.__p2
+
+    def test_7_attribut_change(self):
+        element = Element((0, 0), (10, 0))
+        element.p1 = (10, 0)
+        element.p2 = (50, 0)
+        self.assertTrue(element.p1 == (10, 0) and element.p2 == (50, 0))
+
+    def test_8_attribut_change_error(self):
+        element = Element((0, 0), (10, 0))
+        with self.assertRaises(AssertionError):
+            element.p1 = (30, 30)
+
+    def test_9_get_coord(self):
+        element = Element((0, 0), (10, 0))
+        self.assertTrue(element.get_x_coords() == (0, 10)
+                        and element.get_y_coords() == (0, 0))
+
 
 if __name__ == '__main__':
     unittest.main()
