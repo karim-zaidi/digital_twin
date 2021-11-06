4930e55faed6a277839dd52cfc44b12b8c935db1
karim-zaidi <90897995+karim-zaidi@users.noreply.github.com>
2021-11-06 21:35:03 +0100

    feat: add test

--- code/Door_test.py
+++ code/Door_test.py
@@ -1,6 +1,8 @@
 import unittest
+
 from Door import Door
 from Point import P
+
 
 class Wall_test(unittest.TestCase):
 
@@ -9,6 +11,13 @@ class Wall_test(unittest.TestCase):
         door = Door(p1, p2)
         self.assertIsInstance(door, Door)
         self.assertTrue(door.p1 == p1 and door.p2 == p2)
+
+    def test_1_create_door_error(self):
+        with self.assertRaises(AssertionError):
+            Door(P(0, 0), (1,0))
+        with self.assertRaises(AssertionError):
+            Door([0, 0], (1,0))
+            
 
 if __name__ == '__main__':
     unittest.main()
--- code/Window_test.py
+++ code/Window_test.py
@@ -1,6 +1,8 @@
 import unittest
-from Window import Window
+
 from Point import P
+from Window import Window
+
 
 class Window_test(unittest.TestCase):
 
@@ -9,6 +11,37 @@ class Window_test(unittest.TestCase):
         window = Window(p1, p2)
         self.assertIsInstance(window, Window)
         self.assertTrue(window.p1 == p1 and window.p2 == p2)
+    
 
+    def test_1_create_window_error(self):
+        with self.assertRaises(AssertionError):
+            Window(P(0, 0), (1,0))
+        with self.assertRaises(AssertionError):
+            Window([0, 0], (1,0))
+    
+    def test_2_attribut(self):
+        p1, p2 = P(0, 0), P(1,0)
+        window = Window(p1, p2)
+        
+        # changing attribut
+        new_p1, new_p2 = P(2,0), P(10,0)
+        window.p1, window.p2 = new_p1, new_p2
+
+        self.assertTrue(window.p1 == new_p1 and window.p2 == new_p2)
+
+
+    def test_3_attribut_error(self):
+        p1, p2 = P(0, 0), P(1,0)
+        window = Window(p1, p2)
+        
+        # p1
+        with self.assertRaises(AttributeError):
+            window.__p1
+        
+        # p2
+        with self.assertRaises(AttributeError):
+            window.__p2
+
+    
 if __name__ == '__main__':
     unittest.main()
