7f4d82fdb92ec4a7b04eb5b159f5e76e5c85ce2a
karim-zaidi <90897995+karim-zaidi@users.noreply.github.com>
2021-11-06 21:55:09 +0100

    fix: correct typo in Zone_test

--- code/Zone_test.py
+++ code/Zone_test.py
@@ -3,9 +3,11 @@ from Zone import Zone
 
 class Zone_test(unittest.TestCase):
     
-    def create_empty_zone(self):
+    def test_0_create_empty_zone(self):
         zone = Zone()
         self.assertIsInstance(zone, Zone)
+        self.assertTrue(zone.id == 0)
+        self.assertFalse(zone.is_component)
 
 if __name__ == '__main__':
     unittest.main()
