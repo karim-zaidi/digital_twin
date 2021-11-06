9df96fbcb9a349b674e5bb0351dbb208ae4e4e1b
karim-zaidi <90897995+karim-zaidi@users.noreply.github.com>
2021-11-06 21:37:39 +0100

    feat: add test in Door

--- code/Door.py
+++ code/Door.py
@@ -5,6 +5,6 @@ class Door(Element):
     def __init__(self, p1, p2):
         super().__init__(p1,p2)
 
-
+    # Change the representation into something more readable
     def __repr__(self):
         return f"{self.__class__.__name__}({self.p1}, {self.p2})"
